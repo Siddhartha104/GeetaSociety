cf9e1aba485599e1e51dc367bac77617976bd508
Siddhartha Sen <siddharthasen@Siddharthas-MacBook-Air.local>
2025-07-19 13:08:55 +0530

    Dashboard screen

--- Component/Home/index.js
+++ Component/Home/index.js
@@ -1,26 +1,89 @@
 import React from 'react';
-import { useEffect } from 'react';
-import { View, Text, StyleSheet,SafeAreaView } from 'react-native';
+import { View, Text, StyleSheet, SafeAreaView, ImageBackground, StatusBar, TouchableOpacity } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+
 
 const HomeScreen = () => {
-
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.text}>Hello Home screen</Text>
+      <StatusBar translucent backgroundColor="orange" />
+      <ImageBackground
+        source={require('../../assets/home_background.png')}
+        style={styles.background}
+        resizeMode="cover"
+      >
+        <View style={styles.buttonWrapper}>
+          <TouchableOpacity style={styles.button}>
+             <LinearGradient
+    colors={['#FFD700', '#FFA500']}
+    style={styles.button1}
+  >
+            <Text style={styles.buttonText}>Bhajan</Text>
+            </LinearGradient>
+          </TouchableOpacity>
+
+         <TouchableOpacity style={styles.button}>
+             <LinearGradient
+    colors={['#FFD700', '#FFA500']}
+    style={styles.button1}
+  >
+            <Text style={styles.buttonText}>Sloke</Text>
+            </LinearGradient>
+          </TouchableOpacity>
+
+        </View>
+      </ImageBackground>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, 
-    justifyContent: 'center', 
-    alignItems: 'center',
-    backgroundColor:'#fff'
+    flex: 1,
   },
-  text: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  }
+  background: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonWrapper: {
+    width: '100%',
+    paddingHorizontal: 20,
+    gap: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    width: '80%',
+    height: 55,
+    backgroundColor: '#fff',
+    borderRadius: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    elevation: 5, // Android shadow
+    shadowColor: '#000', // iOS shadow
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+  },
+   button1: {
+    width: '100%',
+    height: 55,
+    backgroundColor: '#fff',
+    borderRadius: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    elevation: 5, // Android shadow
+    shadowColor: '#000', // iOS shadow
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+  },
+  buttonText: {
+    fontSize: 18,
+    fontWeight: '600',
+    color: '#000',
+  },
 });
-export default HomeScreen
+
+export default HomeScreen;
